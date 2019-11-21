a8ad2610e49a9cdb15c33afef24e0417b2f80c20
neeil1990 <neeil@mail.ru>
2019-11-21 16:16:34 +0300

    no message

--- bitrix/templates/hyundai/js/main.js
+++ bitrix/templates/hyundai/js/main.js
@@ -11,3 +11,11 @@ $('#_wJumpToCars').click(function () {
     });
 
 });
+
+window.onload = function() {
+    $('.showroom-car__name').each(function (id, elem) {
+        if($(this).text() == 'i30 N'){
+            $(this).closest('.showroom-car').remove();
+        }
+    });
+};
